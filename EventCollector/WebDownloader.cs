0619ffb49a0db2ee2a82808c284ce1479cac02b6
nakaji <nakaji126@gmail.com>
2013-08-06 01:12:19 +0900

    ダウンロードを行うラッパークラスを追加

---
+++ EventCollector/WebDownloader.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Text;
+
+namespace EventCollector
+{
+    public class WebDownloader
+    {
+        private Encoding _encoding = Encoding.Default;
+
+        public Encoding Encoding
+        {
+            get { return _encoding; } 
+            set { _encoding = value; }
+        }
+
+        public virtual string DownloadString(string url)
+        {
+            using (var cl = new WebClient())
+            {
+                cl.Encoding = Encoding;
+                var str = cl.DownloadString(url);
+                return str;
+            }
+        }
+    }
+}
---
+++ EventCollectorTest/WebDownloaderTest.cs
@@ -0,0 +1,19 @@
+﻿using EventCollector;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EventCollectorTest
+{
+    [TestClass]
+    public class WebDownloaderTest
+    {
+        [TestMethod]
+        public void 指定されたurlから文字列を取得する()
+        {
+            var sut = new WebDownloader();
+
+            var str = sut.DownloadString("http://www.bing.com/");
+
+            str.Contains("<html").IsTrue();
+        }
+    }
+}
