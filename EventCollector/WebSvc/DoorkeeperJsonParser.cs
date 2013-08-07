7943f149301cbc7ee8593b2ce8649a708ef83921
nakaji <nakaji126@gmail.com>
2013-08-08 01:21:46 +0900

    DoorkeeperのJSONデータをインスタンス化する処理を追加

---
+++ EventCollector/WebSvc/DoorkeeperJsonParser.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Codeplex.Data;
+using EventData;
+
+namespace EventCollector.WebSvc
+{
+    public class DoorkeeperJsonParser
+    {
+        public static IList<CommonEvent> Parse(string str)
+        {
+            var events = GetEventList(str);
+            return events;
+        }
+
+        /// <summary>
+        /// Jsonからイベントリストを作成
+        /// </summary>
+        /// <param name="str">Json形式の文字列</param>
+        /// <returns>イベントリスト</returns>
+        private static IList<CommonEvent> GetEventList(string str)
+        {
+            var root = DynamicJson.Parse(str);
+
+            var events = new List<CommonEvent>();
+            foreach (var item in root)
+            {
+                events.Add(new CommonEvent(
+                    item.@event.title,
+                    ParseDateTime(item.@event.starts_at),
+                    ParseDateTime(item.@event.ends_at),
+                    item.@event.address,
+                    item.@event.venue_name,
+                    item.@event.description,
+                    item.@event.group.name,
+                    item.@event.group.public_url,
+                    item.@event.public_url
+                    ));
+            }
+            return events;
+        }
+
+        public static DateTime? ParseDateTime(string str)
+        {
+            DateTime wk;
+            if (DateTime.TryParse(str, out wk))
+            {
+                return wk;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+    }
+}
---
+++ EventCollectorTest/WebSvc/DoorkeeperJsonParserTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventCollector;
+using EventCollector.WebSvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EventCollectorTest.WebSvc
+{
+    [TestClass]
+    public class DoorkeeperJsonParserTest
+    {
+        private DoorkeeperJsonParser sut;
+
+        [TestMethod]
+        public void Jsonから共通イベントオブジェクトを作成する()
+        {
+            var str = File.ReadAllText(@"TestData\Doorkeeper-松山-201307.json");
+
+            var events = DoorkeeperJsonParser.Parse(str);
+
+            events.Count().Is(1);
+            events.ToArray()[0].Title.Is("Agile459 アジャイルサムライ読書会松山道場 #15 『ラストサムライ』");
+            ;
+        }
+    }
+}
