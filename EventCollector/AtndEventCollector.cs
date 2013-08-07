c17174f972b8fcb42c065f26c8635eeae62aaeec
nakaji <nakaji126@gmail.com>
2013-08-07 23:09:49 +0900

    ATND検索用クラスを追加

---
+++ EventCollector/AtndEventCollector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EventCollector.WebSvc;
+using EventData;
+
+namespace EventCollector
+{
+    public class AtndEventCollector
+    {
+        private const string BaseUrl = "http://api.atnd.org/events/?format=json";
+        private const int ReadCount = 25;
+
+        public IList<CommonEvent> GetEvents(int ym, string keyword)
+        {
+            var apiUrl = string.Format(BaseUrl + "&count={0}&keyword={1}&ym={2}", ReadCount, keyword, ym);
+
+            var downloader = new WebDownloader();
+            var str = downloader.DownloadString(apiUrl);
+
+            return AtndJsonParser.Parse(str);
+        }
+    }
+}
---
+++ EventCollectorTest/WebSvc/AtndEventCollectorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EventCollector;
+
+namespace EventCollectorTest.WebSvc
+{
+    [TestClass]
+    public class AtndEventCollectorTest
+    {
+        [TestMethod]
+        public void イベント取得()
+        {
+            var sut = new AtndEventCollector();
+            var result = sut.GetEvents(201307, "松山");
+
+            result.Count().IsNot(0);
+            // 2013年7月にかからないイベントは存在しない
+            result.Any(x => x.EndedAt < new DateTime(2013, 07, 01) && x.StartedAt >= new DateTime(2013, 08, 01)).IsFalse();
+
+        }
+    }
+}
