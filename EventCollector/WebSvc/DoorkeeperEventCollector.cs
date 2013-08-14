6b98df39d86d047ee8ff4c6a10f442f7ce8f3830
nakaji <nakaji126@gmail.com>
2013-08-15 00:48:31 +0900

    Doorkeeper検索用クラスを追加

---
+++ EventCollector/WebSvc/DoorkeeperEventCollector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EventData;
+
+namespace EventCollector.WebSvc
+{
+    public class DoorkeeperEventCollector
+    {
+        private const string BaseUrl = "http://api.doorkeeper.jp/events?sort=starts_at&locale=ja";
+
+        public int ReadCount { get; private set; }
+
+        public DoorkeeperEventCollector()
+        {
+            ReadCount = 25;
+        }
+
+        public IList<CommonEvent> GetEvents(int ym, string keyword)
+        {
+            var since = new DateTime((int)Math.Floor(ym / (decimal)100), ym % 100, 1);
+            var until = since.AddMonths(1).AddDays(-1);
+            var apiUrl = string.Format(BaseUrl + "&since={0}&until={1}&q={2}", since.ToString("O"), until.ToString("O"), keyword);
+
+            var downloader = new WebDownloader {Encoding = Encoding.UTF8};
+
+            var str = downloader.DownloadString(apiUrl);
+
+            return DoorkeeperJsonParser.Parse(str);
+        }
+    }
+}
---
+++ EventCollectorTest/WebSvc/DoorkeeperEventCollectorTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventCollector.WebSvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EventCollector;
+
+namespace EventCollectorTest.WebSvc
+{
+    [TestClass]
+    public class DoorkeeperEventCollectorTest
+    {
+        [TestClass]
+        public class コンストラクタ
+        {
+            [TestMethod]
+            public void デフォルトでは読み込み件数は25件()
+            {
+                var sut = new DoorkeeperEventCollector();
+
+                sut.ReadCount.Is(25);
+            }
+        }
+
+        [TestMethod]
+        public void イベント取得()
+        {
+            var sut = new DoorkeeperEventCollector();
+            var result = sut.GetEvents(201307, "松山");
+
+            result.Count().IsNot(0);
+            // 2013年7月にかからないイベントは存在しない
+            result.Any(x => x.EndedAt < new DateTime(2013, 07, 01) && x.StartedAt >= new DateTime(2013, 08, 01)).IsFalse();
+        }
+    }
+}
