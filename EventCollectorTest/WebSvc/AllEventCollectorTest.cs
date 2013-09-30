1970224b6baf71a3e2bbf7aec1c336a355169b66
nakaji <nakaji126@gmail.com>
2013-09-30 23:31:17 +0900

    NMock3の導入

--- EventCollectorTest/WebSvc/AllEventCollectorTest.cs
+++ EventCollectorTest/WebSvc/AllEventCollectorTest.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EventCollector.WebSvc;
+using EventData;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NMock;
 
 namespace EventCollectorTest.WebSvc
 {
@@ -18,6 +20,27 @@ namespace EventCollectorTest.WebSvc
             var result = sut.GetEvents(201307, "松山");
 
             result.Count().Is(4);
+        }
+        
+        [TestMethod]
+        public void NMock()
+        {
+            var factory = new MockFactory();
+            var mock = factory.CreateMock<AtndEventCollector>();
+
+            var result = new List<CommonEvent>();
+            result.AddRange(new CommonEvent[]
+                      {
+                          new CommonEvent("A", null, null, "", "", "", "", "", ""),
+                          new CommonEvent("A", null, null, "", "", "", "", "", ""),
+                      });
+
+
+            mock.Expects.One.MethodWith(x => x.GetEvents(201307, "松山")).WillReturn(result);
+
+            var sut = mock.MockObject;
+
+            sut.GetEvents(201307, "松山").Count();
         }
 
     }
