7f0182416c729c4baeca3f1a71fafa03daa3dfa4
nakaji <nakaji126@gmail.com>
2013-10-04 01:10:50 +0900

    lessを導入

--- EventSearch/App_Start/BundleConfig.cs
+++ EventSearch/App_Start/BundleConfig.cs
@@ -23,7 +23,9 @@ namespace EventSearch
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/site.css",
+                        "~/Content/custom.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
