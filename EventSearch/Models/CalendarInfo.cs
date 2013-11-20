55c8275e1c077f6072aea0b82559d24a81242238
nakaji <nakaji126@gmail.com>
2013-11-21 01:00:13 +0900

    登録するカレンダーを選択できるようにした
    refs #6

---
+++ EventSearch/Models/CalendarInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventSearch.Models
+{
+    public class CalendarInfo
+    {
+        public string Id { get; set; }
+        public string Summary { get; set; }
+    }
+}
