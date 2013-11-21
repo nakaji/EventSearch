0db9ca9b9df7bbbdd082aa1ac124880fb339e5e4
nakaji <nakaji126@gmail.com>
2013-11-22 01:37:54 +0900

    認証用のコントローラを抽出 refs #8

--- EventSearch/Controllers/HomeController.cs
+++ EventSearch/Controllers/HomeController.cs
@@ -60,41 +60,5 @@ namespace EventSearch.Controllers
 
             return Redirect("~/");
         }
-
-        public ActionResult Login()
-        {
-            var clientId = ConfigurationManager.AppSettings.Get("client_id");
-            var redirectUri = ConfigurationManager.AppSettings.Get("redirect_uri");
-            var scope = HttpUtility.UrlEncode("https://www.googleapis.com/auth/calendar") + "+" +
-                        HttpUtility.UrlEncode("https://www.googleapis.com/auth/userinfo.profile");
-
-            var addr = string.Format("https://accounts.google.com/o/oauth2/auth?client_id={0}&response_type=code&redirect_uri={1}&scope={2}",
-                                    clientId,
-                                    redirectUri,
-                                    scope
-                                );
-
-            return Redirect(addr);
-        }
-
-        public ActionResult Logout()
-        {
-            Session["access_token"] = null;
-
-            return Redirect("~/");
-        }
-
-        public ActionResult Auth(string code)
-        {
-            var apis = new GoogleApis();
-
-            apis.Auth(code);
-            Session.Add("access_token", apis.AccessToken);
-
-            var userInfo = apis.GetUserInfo();
-            Session.Add("user_info", userInfo);
-
-            return Redirect("~/");
-        }
     }
 }
